ef193c8b77e54925ece26f9d3e8ec9092869c962
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2023-02-13 01:13:56 +0530

    download link added

--- componnts/Resume/MainRes.js
+++ componnts/Resume/MainRes.js
@@ -1,13 +1,20 @@
+import { useRouter } from 'next/router';
 import Button2 from '../UI/Buttons/Button2';
 import Resume from '../../resources/Resume.jpg';
 
 import classes from '../../styles/Resume/MainRes.module.css';
 
 export default function MainRes() {
+  const Router = useRouter();
+  function download() {
+    Router.push(
+      'https://drive.google.com/uc?export=download&id=1V0TGs7LRZLGS5Mg3SvrTSbJcy36HWmmn'
+    );
+  }
   return (
     <div className={classes.resCont}>
       <div className={classes.downl}>
-        <Button2 text="Download" />
+        <Button2 text="Download" method={download} />
       </div>
       <div className={classes.image}>
         <img src={Resume.src} alt="" />
