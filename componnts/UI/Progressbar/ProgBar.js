3da04d958e894cd726f9a3dc7896335c7d847e3d
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2023-03-25 21:45:05 +0530

    progress bar finalized

--- componnts/UI/Progressbar/ProgBar.js
+++ componnts/UI/Progressbar/ProgBar.js
@@ -1,32 +1,35 @@
 import { easeIn, motion } from 'framer-motion';
 
-import classes from './ProgBar.module.css';
+import classes from '../../../styles/UI/Progressbar/ProgBar.module.css';
 
 export default function ProgBar(props) {
   return (
-    <div className={classes.barbottom}>
-      <motion.div
-        className={classes.bartop}
-        initial={{
-          opacity: 1,
-          width: '0%',
-          color: 'white',
-          y: 0,
-        }}
-        whileInView={{ width: props.progress + '%' }}
-        transition={{ delay: 0.5, type: easeIn }}
-        viewport={{ once: true }}
-      >
-        <motion.h3
-          inital={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ delay: 3 }}
+    <div className={classes.barparent}>
+      <div className={classes.barbottom}>
+        <motion.div
+          className={classes.bartop}
+          initial={{
+            opacity: 1,
+            width: '0%',
+            color: 'white',
+            y: 0,
+          }}
+          whileInView={{ width: props.progress + '%' }}
+          transition={{ delay: 0.5, type: 'easeIn' }}
           viewport={{ once: true }}
-          className={classes.per}
         >
-          {props.progress + '%'}
-        </motion.h3>
-      </motion.div>
+          {/* <motion.h3
+            inital={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+            viewport={{ once: true }}
+            className={classes.per}
+          >
+            {props.progress + '%'}
+          </motion.h3> */}
+        </motion.div>
+      </div>
+      <h5 className={classes.per}>{props.progress + '%'}</h5>
     </div>
   );
 }
