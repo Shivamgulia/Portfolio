033533ddb4e02d7cc0da12e9a1114f2cfa331fb8
shivamgulia <shivam.meert@gmail.com>
2024-08-11 13:15:46 +0530

    routing and pages added

---
+++ components/main/Experience.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "@/styles/components/main/Experience.module.css";
+
+function Experience() {
+  return <div>Experience</div>;
+}
+
+export default Experience;
---
+++ components/main/Resume.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "@/styles/components/main/Resume.module.css";
+
+function Resume() {
+  return <div>Resume</div>;
+}
+
+export default Resume;
