12d9bd1098f361d13a85c11f5adec6bc3b2deb51
shivamgulia <shivam.meert@gmail.com>
2024-08-13 21:16:25 +0530

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import React from "react";
 import { Suspense } from "react";
 
 import Main from "@/components/main/Main";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Suspense>
+      <Head>
+        <title>Shivam Gulia</title>
+      </Head>
       <Main />
     </Suspense>
   );
